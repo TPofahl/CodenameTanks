5823719c5030f6dddac9b2497aa87c00641a053b
TPofahl <thomaspofahl@yahoo.com>
2022-12-23 16:59:17 -0600

    Added camera zoom to player's tank

--- Code/Tank.cs
+++ Code/Tank.cs
@@ -6,6 +6,15 @@ public partial class Tank : RigidBody2D
 {
     public TankOptions Type { get; private set; }
     public TankStats Stats { get; private set; }
+    public Camera2D cameraPositon;
+    [Export]
+    public Vector2 zoomSpeed;
+    [Export]
+    public Vector2 minZoom;
+     [Export]
+    public Vector2 maxZoom;
+
+
 
     private static Tank New()
     {
@@ -24,6 +33,7 @@ public partial class Tank : RigidBody2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        cameraPositon = GetNode<Camera2D>("Camera2D");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -34,10 +44,14 @@ public partial class Tank : RigidBody2D
         var tankPos = GlobalPosition;
         var angle = mousePos.AngleToPoint(tankPos);
         turret.Rotation = angle;
-
     }
+    
     public override void _PhysicsProcess(double delta)
     {
+        zoomSpeed = new Vector2(0.70F, 0.70F);
+        minZoom = new Vector2(0.4F, 0.4F);
+        maxZoom = new Vector2(2.50F, 2.50F);
+
         if (Input.IsActionPressed("right"))
         {
             var force = GlobalTransform.x * (float)delta * Stats.Speed;
@@ -49,6 +63,16 @@ public partial class Tank : RigidBody2D
             var force = -GlobalTransform.x * (float)delta * Stats.Speed;
             GD.Print($"Left: {force}");
             ApplyCentralImpulse(force);
+        }
+        if (Input.IsActionJustReleased("scrollUp") && cameraPositon.Zoom.x <= maxZoom.x) 
+        {
+            GD.Print($"Scroll up: {cameraPositon.Zoom}");
+            cameraPositon.Zoom += zoomSpeed;
+        }
+        if (Input.IsActionJustReleased("scrollDown") && cameraPositon.Zoom.x >= minZoom.x) 
+        {
+            GD.Print($"Scroll down: {cameraPositon.Zoom}");
+            cameraPositon.Zoom -= zoomSpeed;
         }
     }
 }
